52272e1bf5b8b0f6142744d9433070bff4c90a0e
EngSolimanSayed <soliman.sayed.helwan@gmail.com>
2025-10-04 18:36:03 +0300

    Add Solving_SheetsPl2 project with Sheet1

---
+++ Sheet1/Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+    public static void main(String[] args) {
+        float width = 4.5f;
+        float height = 7.9f;
+        float area = width * height;
+        float perimeter = (width + height) * 2;
+        System.out.println("Area is: " + area);
+        System.out.println("Perimeter is: " + perimeter);
+
+    }
+
+}
